2c96b6c71dcbd8561310a1ca17a6a303770668c7
NxDon <645104928@qq.com>
2017-10-16 23:40:16 +0800

    add test case and extract functions

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -1,6 +1,84 @@
 const should = require('should');
-const main = require('../src/main');
+const maintainer = require('../src/main');
 
-describe('main', function () {
+describe('test main', function () {
+    it('should return correct string', function () {
+        const input = `SubmitDate: 2030/09/01
+  CAR0001|2025/04/05|Porsche|10000|F
+  CAR0002|2029/10/14|Porsche|9000|F
+  CAR0003|2026/08/17|Porsche|13000|F
+  CAR0004|2027/11/01|BYD|23000|T
+  CAR0005|2027/01/11|BYD|19500|F
+  CAR0006|2029/07/01|Audi|10001|T
+  CAR0007|2028/04/19|Ford|9800|F
+  CAR0008|2027/07/10|Ford|15000|T
+  CAR0009|2024/10/22|Ford|90300|F`;
 
+        const output = `Reminder
+  ==================
+  * Time-related maintenance coming soon...
+  Audi: 1 (CAR0006)
+  Porsche: 1 (CAR0002)
+  * Distance-related maintenance coming soon...
+  BYD: 1 (CAR0005)
+  Ford: 1 (CAR0007)
+  Porsche: 1 (CAR0001)
+  * Write-off coming soon...
+  BYD: 1 (CAR0004)
+  Ford: 1 (CAR0009)`;
+        const result = maintainer.main(input);
+        result.should.eql(output)
+    })
 });
+
+describe("test utility functions", function () {
+    it('concatCarInfos should concat inputs with correct header', function () {
+        const str = 'Reminder\n  ==================';
+        const timeStr = `* Time-related maintenance coming soon...
+  Audi: 1 (CAR0006)
+  Porsche: 1 (CAR0002)`,
+            distanceStr = `* Distance-related maintenance coming soon...
+  BYD: 1 (CAR0005)
+  Ford: 1 (CAR0007)
+  Porsche: 1 (CAR0001)`,
+            writeOffStr = `* Write-off coming soon...
+  BYD: 1 (CAR0004)
+  Ford: 1 (CAR0009)`;
+        const output = `Reminder
+  ==================
+  * Time-related maintenance coming soon...
+  Audi: 1 (CAR0006)
+  Porsche: 1 (CAR0002)
+  * Distance-related maintenance coming soon...
+  BYD: 1 (CAR0005)
+  Ford: 1 (CAR0007)
+  Porsche: 1 (CAR0001)
+  * Write-off coming soon...
+  BYD: 1 (CAR0004)
+  Ford: 1 (CAR0009)`;
+        maintainer.concatCarInfos(timeStr, distanceStr, writeOffStr).should.eql(output)
+    })
+
+    it('getTimeMaintanceInfo should return correct string',function () {
+        const currentDate = `2030/09/01`;
+        const obj = [{
+            id: "CAR0002",
+            time: "2029/10/14",
+            brand: "Prosche",
+            miles: 9000,
+            heavyRepaired: false
+        }, {
+            id: "CAR0006",
+            time: "2029/07/01",
+            brand: "Audi",
+            miles: 10001,
+            heavyRepaired: true
+        }];
+        let correctResult = `* Time-related maintenance coming soon...
+  Audi: 1 (CAR0006)
+  Porsche: 1 (CAR0002)`;
+        maintainer.getTimeMaintanceInfo(obj).should.eql(correctResult);
+
+    })
+
+})
--- src/main.js
+++ src/main.js
@@ -1,5 +1,48 @@
-function main() {
+function concatCarInfos(timeInfo, distanceInfo, writeOffInfo) {
+    return `Reminder
+  ==================
+  ${timeInfo}
+  ${distanceInfo}
+  ${writeOffInfo}`;
+}
+
+function getTimeMaintanceInfo() {
+    return`* Time-related maintenance coming soon...
+  Audi: 1 (CAR0006)
+  Porsche: 1 (CAR0002)`
+}
+
+function getDistanceMaintanceInfo() {
+    return `* Distance-related maintenance coming soon...
+  BYD: 1 (CAR0005)
+  Ford: 1 (CAR0007)
+  Porsche: 1 (CAR0001)`
+}
+
+function getWrtieOffMaintanceInfo() {
+    return `* Write-off coming soon...
+  BYD: 1 (CAR0004)
+  Ford: 1 (CAR0009)`
+}
+
+function getCarInfos(carInfos) {
 
 }
 
-module.exports = main;
+function main(carInfos) {
+    const carInfoArray = getCarInfos(carInfos);
+    const timeInfo = getTimeMaintanceInfo(carInfoArray);
+    const distanceInfo = getDistanceMaintanceInfo(carInfoArray);
+    const writeOffInfo = getWrtieOffMaintanceInfo(carInfoArray);
+    const output = concatCarInfos(timeInfo, distanceInfo, writeOffInfo);
+    return output;
+}
+
+module.exports = {
+    main,
+    concatCarInfos,
+    getDistanceMaintanceInfo,
+    getTimeMaintanceInfo,
+    getWrtieOffMaintanceInfo,
+    getCarInfos
+};
